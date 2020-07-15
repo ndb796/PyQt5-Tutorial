1a4c6f1d40b851d5fb346f45c2e354376b425d58
Dongbin Na <ndb796@naver.com>
2020-07-15 14:48:39 +0900

    Create main.py

---
+++ 1_basic/2/main.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import *
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.setWindowTitle("클래스로 정의한 화면")
+        button = QPushButton("Push me!", self)
+        button.clicked.connect(self.button_clicked)
+
+    def button_clicked(self):
+        QMessageBox.information(self, "Information", "You clicked me!")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    my_window = MyWindow()
+    my_window.show()
+    app.exec_()
