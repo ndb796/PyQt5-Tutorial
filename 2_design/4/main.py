9038fbf0b7aecfd915a91f8e176634e7f142b999
Dongbin Na <ndb796@naver.com>
2020-07-15 15:43:48 +0900

    Create main.py

---
+++ 2_design/4/main.py
@@ -0,0 +1,45 @@
+import sys
+from PyQt5.QtWidgets import *
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setStyleSheet("""
+            QWidget {
+                background: beige;
+            }
+            QPushButton {
+                background-color: white;
+            }
+            QPushButton#confirm_button {
+                background-color: red;
+            }
+        """)
+
+        default_widget = DefaultWidget()
+        self.setCentralWidget(default_widget)
+
+
+class DefaultWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        label = QLabel('Do you like chicken?')
+        ok_button = QPushButton('Yes')
+        ok_button.setObjectName("confirm_button")
+        cancel_button = QPushButton('No')
+        
+        layout = QHBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(ok_button)
+        layout.addWidget(cancel_button)
+        self.setLayout(layout)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    my_window = MyWindow()
+    my_window.show()
+    app.exec_()
