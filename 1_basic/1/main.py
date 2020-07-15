6a70eee8c5255a80b9eb6bfe058c5fa992382a51
Dongbin Na <ndb796@naver.com>
2020-07-15 14:46:07 +0900

    Create main.py

---
+++ 1_basic/1/main.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.QtWidgets import *
+
+
+# Callback 함수
+def clicked_slot():
+    print('clicked')
+
+
+app = QApplication(sys.argv)
+
+# Button 위젯
+btn = QPushButton("Push me!")
+btn.clicked.connect(clicked_slot)
+btn.show()
+
+# GUI 프로그램 동작 시작
+print("이벤트 루프가 시작됩니다...")
+app.exec_()
+print("이벤트 루프를 종료됩니다...")
